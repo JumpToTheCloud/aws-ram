d91e53ec3dcaf6afb43cff5f91feea33fb61f508
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2025-01-30 12:23:49 +0100

    chore(projen): repository configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,97 @@
-import { awscdk } from 'projen';
+import { awscdk, ReleasableCommits, SampleFile } from 'projen';
+import { GithubCredentials } from 'projen/lib/github';
+import { AppPermission } from 'projen/lib/github/workflows-model';
+import { TrailingComma } from 'projen/lib/javascript';
+import { VsCode } from 'projen/lib/vscode';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Antonio Márquez Pérez',
   authorAddress: 'antonio.marquez@jumptothecloud.tech',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.173.0',
+  constructsVersion: '10.4.2',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.7.0',
   name: 'aws-ram',
+  keywords: ['aws', 'cdk', 'ram', 'resource access manager'],
   projenrcTs: true,
   repositoryUrl: 'git@github.com:JumpToTheCloud/aws-ram.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      trailingComma: TrailingComma.ES5,
+      singleQuote: true,
+      bracketSpacing: true,
+      semi: true,
+    },
+  },
+  autoMerge: false,
+  releasableCommits: ReleasableCommits.featuresAndFixes(),
+  publishDryRun: true,
+  description: 'AWS CDK L2 construct for Resource Access Manager',
+  devDeps: ['commitizen', 'cz-customizable', 'jest-runner-groups'],
+  packageName: '@jttc/aws-ram',
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp({
+      permissions: {
+        pullRequests: AppPermission.WRITE,
+        contents: AppPermission.WRITE,
+      },
+    }),
+  },
 });
+
+project.addTask('commit', {
+  description:
+    'Commit changes with conventional commits prompts provided by Commitizen',
+  steps: [
+    {
+      exec: './node_modules/cz-customizable/standalone.js',
+      receiveArgs: false,
+      say: 'committing changes',
+    },
+  ],
+});
+
+const snapsthoTest = project.addTask('test:snapshot', {
+  description: 'Snapshots Tests',
+  steps: [
+    {
+      exec: 'jest --group=snapshot --collectCoverage=false',
+      say: 'Snapshots Tests',
+      receiveArgs: true,
+    },
+  ],
+});
+
+project.addTask('test:snapshot:update', {
+  description: 'Update snapshots',
+  steps: [
+    {
+      exec: 'jest --updateSnapshot --collectCoverage=false',
+      say: 'Updating snapshots',
+      receiveArgs: true,
+    },
+  ],
+});
+
+const testTask = project.tasks.tryFind('test');
+const eslintTask = project.tasks.tryFind('eslint');
+
+if (testTask && eslintTask) {
+  testTask.reset();
+  testTask.spawn(snapsthoTest);
+  testTask.spawn(eslintTask);
+}
+
+new SampleFile(project, '.nvmrc', {
+  contents: 'v22.12.0',
+});
+
+const vscode = new VsCode(project);
+vscode.settings.addSettings({
+  'editor.formatOnSave': true,
+  'editor.indentSize': 2,
+  'editor.defaultFormatter': 'esbenp.prettier-vscode',
+});
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@jttc/aws-ram.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@jttc/aws-ram.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@jttc/aws-ram'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@jttc/aws-ram.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@jttc/aws-ram.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
